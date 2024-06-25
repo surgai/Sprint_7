742054f517b36c4d882cba6f281e45f7eaf1f36a
Nuikin Denis <dnsurgai@yandex.ru>
2024-06-25 17:12:23 +0400

    new test

--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -21,23 +21,17 @@ class TestCreateCourier:
     @allure.title('Нельзя создать двух одинаковых курьеров с одинаковыми логинами')
     def test_courier_was_created(self):
         CREATE_COURIER = '/api/v1/courier'
-        response = requests.post(
-            f'{Urls.URL}{CREATE_COURIER}',
-            TestCreateCourier.data)
+        response = requests.post(f'{Urls.URL}{CREATE_COURIER}', TestCreateCourier.data)
         assert response.status_code == 409 and 'Этот логин уже используется' in response.text
 
     @allure.title('Нельзя создать курьера без логина')
     def test_create_courier_without_login(self):
         CREATE_COURIER = '/api/v1/courier'
-        response = requests.post(
-            f'{Urls.URL}{CREATE_COURIER}',
-            gen_without_login())
+        response = requests.post(f'{Urls.URL}{CREATE_COURIER}', gen_without_login())
         assert response.status_code == 400 and 'Недостаточно данных для создания учетной записи' in response.text
 
     @allure.title('Нельзя создать курьера без пароля')
     def test_create_courier_without_password(self):
         CREATE_COURIER = '/api/v1/courier'
-        response = requests.post(
-            f'{Urls.URL}{CREATE_COURIER}',
-            gen_without_password())
+        response = requests.post(f'{Urls.URL}{CREATE_COURIER}', gen_without_password())
         assert response.status_code == 400 and 'Недостаточно данных для создания учетной записи' in response.text
--- tests/test_list_order.py
+++ tests/test_list_order.py
@@ -6,6 +6,7 @@ from urls import Urls
 class TestReturnOrderList:
     @allure.title('В тело ответа возвращается список заказов')
     def test_list_order(self):
-        CREATE_ORDER= '/api/v1/courier'
-        response = requests.get(f'{Urls.URL}{CREATE_ORDER}')
+        LIST_ORDER = '/api/v1/orders?courierId=334509'
+        response = requests.get(f'{Urls.URL}{LIST_ORDER}')
+        print(response.text)
         assert response.status_code == 200 and "orders" in response.json()
