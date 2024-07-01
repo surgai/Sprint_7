1352daef804f4c8cf69e488d4359213ac60fee27
Nuikin Denis <dnsurgai@yandex.ru>
2024-07-01 21:06:26 +0400

    move assert back to test

--- pages/page_api.py
+++ pages/page_api.py
@@ -5,29 +5,20 @@ from generation import register_new_courier_and_return_login_password as gen
 from generation import register_new_courier_without_login as gen_without_login
 from generation import register_new_courier_without_login as gen_without_password
 from constanta import Handle
-from http import HTTPStatus
+
 
 
 class ApiPage:
     data = gen()
 
-
     def test_create_courier(self):
-        response_body = '{"ok":true}'
-        response = requests.post(f'{Urls.URL}{Handle.CREATE_COURIER}', ApiPage.data)
-        assert response.status_code == HTTPStatus.CREATED.value and response.text == response_body
-
+        return requests.post(f'{Urls.URL}{Handle.CREATE_COURIER}', ApiPage.data)
 
     def test_courier_was_created(self):
-        response = requests.post(f'{Urls.URL}{Handle.CREATE_COURIER}', ApiPage.data)
-        assert response.status_code == HTTPStatus.CONFLICT.value and 'Этот логин уже используется' in response.text
-
+        return requests.post(f'{Urls.URL}{Handle.CREATE_COURIER}', ApiPage.data)
 
     def test_create_courier_without_login(self):
-        response = requests.post(f'{Urls.URL}{Handle.CREATE_COURIER}', gen_without_login())
-        assert response.status_code == HTTPStatus.BAD_REQUEST.value and 'Недостаточно данных для создания учетной записи' in response.text
-
+        return requests.post(f'{Urls.URL}{Handle.CREATE_COURIER}', gen_without_login())
 
     def test_create_courier_without_password(self):
-        response = requests.post(f'{Urls.URL}{Handle.CREATE_COURIER}', gen_without_password())
-        assert response.status_code == HTTPStatus.BAD_REQUEST.value and 'Недостаточно данных для создания учетной записи' in response.text
+        return requests.post(f'{Urls.URL}{Handle.CREATE_COURIER}', gen_without_password())
--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -1,19 +1,24 @@
 import allure
 from pages.page_api import ApiPage
+from pages.page_api import gen
+from http import HTTPStatus
 
 class TestCreateCourier:
-
+    data = gen()
     @allure.title('Создание курьера')
     def test_create_courier(self):
-        ApiPage.test_create_courier(self)
-
+        response_body = '{"ok":true}'
+        response = ApiPage.test_create_courier(self)
+        assert response.status_code == HTTPStatus.CREATED.value and response.text == response_body
     @allure.title('Нельзя создать двух одинаковых курьеров с одинаковыми логинами')
     def test_courier_was_created(self):
-        ApiPage.test_courier_was_created(self)
-
+        response = ApiPage.test_courier_was_created(self)
+        assert response.status_code == HTTPStatus.CONFLICT.value and 'Этот логин уже используется' in response.text
     @allure.title('Нельзя создать курьера без логина')
     def test_create_courier_without_login(self):
-        ApiPage.test_create_courier_without_login(self)
+        response = ApiPage.test_create_courier_without_login(self)
+        assert response.status_code == HTTPStatus.BAD_REQUEST.value and 'Недостаточно данных для создания учетной записи' in response.text
     @allure.title('Нельзя создать курьера без пароля')
     def test_create_courier_without_password(self):
-        ApiPage.test_create_courier_without_password(self)
+        response = ApiPage.test_create_courier_without_password(self)
+        assert response.status_code == HTTPStatus.BAD_REQUEST.value and 'Недостаточно данных для создания учетной записи' in response.text
